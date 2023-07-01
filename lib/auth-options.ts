09670866b7bd49224d4a8f496335fd18bd08bcce
Prakash <qxprakash@gmail.com>
2023-07-01 23:51:13 +0530

    next auth

---
+++ lib/auth-options.ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { prisma } from "@/prisma/db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      profile(profile) {
+        return {
+          id: String(profile.id),
+          name: profile.login,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    session: async ({session , user}) => {
+        if(session?.user){
+            session.user.id = user.id;
+        }
+        return session;
+    }
+  }
+};
---
+++ prisma/db.ts
@@ -0,0 +1,11 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const globalPrisma = global as unknown as {prisma: PrismaClient};
+
+export const prisma = globalPrisma.prisma || new PrismaClient();
+
+if(process.env.NODE_ENV !== "production"){
+    globalPrisma.prisma = prisma;
+}
+
+
