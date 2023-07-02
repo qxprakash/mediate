13c21fc88dc20c19702f0ed2e0d21b497d660f52
Prakash <qxprakash@gmail.com>
2023-07-02 20:31:05 +0530

    router changed to next/navigation

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,32 +1,66 @@
-"use client"
-import { useSession } from "next-auth/react";
+"use client";
+import { useRouter } from "next/navigation";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 
-export function SiteHeader(){
-    const {data: session} = useSession();
+
+export function SiteHeader() {
+  const { data: session } = useSession();
+  return (
+    <header>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between lg:px-8">
+        <div className="flex items-center gap-x-12">
+          <Link href="/" className="text-xl font-bold">
+            Mediate
+          </Link>
+          <Link
+            href="/new-post"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            New Post
+          </Link>
+        </div>
+        <span className="flex items-center space-x-4">
+          {session?.user && (
+            <Avatar>
+              <AvatarImage src={session.user.image} alt="@shadcn"/>
+                <AvatarFallback>{session.user.name}</AvatarFallback>
+            </Avatar>
+          )}
+          <span>{session ? <SignOut /> : <Login />}</span>
+        </span>
+      </nav>
+    </header>
+  );
+}
+
+function Login() {
+  return (
+    <button
+      onClick={() => signIn()}
+      className="text-sm font-semibold loading-6 text-gray-900"
+    >
+      Log in
+    </button>
+  );
+}
+
+
+function SignOut(){
+    const router = useRouter()
+
+    async function onSignOut(){
+        await signOut();
+        router.push("/api/auth/signin");
+    }
     return (
-        <header>
-            <nav className="mx-auto flex max-w-7xl items-center justify-between lg:px-8">
-                <div className="flex items-center gap-x-12">
-                    <Link href="/" className="text-xl font-bold">
-                        Mediate
-                    </Link>
-                    <Link href="/new-post" className="text-sm font-semibold leading-6 text-gray-900">
-                        New Post
-                    </Link>
-                </div>
-                <span className="flex items-center space-x-4">
-                    {session?.user && (
-                        <Avatar>
-                            <AvatarImage src={session.user.image} alt="@shadcn">
-                                <AvatarFallback></AvatarFallback>
-                            </AvatarImage>
-                        </Avatar>
-                    )}
-                </span>
-                </nav>
-        </header>
-    )
+        <button
+          onClick={() => onSignOut()}
+          className="text-sm font-semibold loading-6 text-gray-900"
+        >
+          Sign Out
+        </button>
+      );
 }
